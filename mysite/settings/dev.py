dda6b2d5ad070e5d1be4dbdc945ae87ba0590dc7
Michael <ayenimichael92@gmail.com>
2026-04-13 12:01:42 +0200

    set ALLOWED_HOSTS to wild card *

--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -4,4 +4,7 @@ DEBUG = True
 
 REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'].append('rest_framework.renderers.BrowsableAPIRenderer')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
+ALLOWED_HOSTS = ['*']
